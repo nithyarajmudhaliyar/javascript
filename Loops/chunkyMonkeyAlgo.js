c48537ec8adaf020f8525f390c4af1faec099bc5
Nithyaraj <nithyarajmudhaliyar2007@gmail.com>
2025-11-30 22:57:23 +0530

    Implement chunkArrayInGroups function

---
+++ Loops/chunkyMonkeyAlgo.js
@@ -0,0 +1,18 @@
+function chunkArrayInGroups(arr,size){
+  let result = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    result.push(arr.slice(i, i + size));
+  }
+
+  return result;
+}
+
+//Testing
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
+console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
