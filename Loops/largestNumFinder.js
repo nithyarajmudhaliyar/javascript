45f234f0d91187c10f005a38417e9b4f52ebe138
Nithyaraj <nithyarajmudhaliyar2007@gmail.com>
2025-12-03 15:31:20 +0530

    Create largestNumFinder.js

---
+++ Loops/largestNumFinder.js
@@ -0,0 +1,17 @@
+function largestOfAll(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        let max = arr[i][0];
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > max) {
+                max = arr[i][j];
+            }
+        }
+        result.push(max);
+    }
+    return result;
+}
+//Testing
+console.log(largestOfAll([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfAll([[13, 27, 18, 26], [4, 5, 1, 3], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
+console.log(largestOfAll([[17, 23, 25, 12], [25, 7, 34, 48], [4, -10, 18, 21], [-72, -3, -17, -10]]));
