fcb6c469167674ff66edc88b6bdbb732d3d5bc7f
Nithyaraj <nithyarajmudhaliyar2007@gmail.com>
2025-12-03 16:41:21 +0530

    Add findElement function to locate first matching element
    Implement a function to find the first element in an array that satisfies a given condition.

---
+++ Loops/firstElementFinder.js
@@ -0,0 +1,17 @@
+function findElement(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            return arr[i];
+        }
+    }
+    return undefined;
+}
+
+//Testing
+console.log(findElement([1, 3, 5, 8, 9, 10], function(num) { return num % 2 === 0; }));
+console.log(findElement([1, 3, 5, 9], function(num) { return num % 2 === 0; }))
+console.log(findElement([1, 2, 3, 4], function(num) { return num > 2; }))
+console.log(findElement(["hello", "world", "javascript"], function(str) { return str.length > 5; }))
+console.log(findElement(["cat", "dog", "bird"], function(str) { return str.length > 10; }))
+console.log(findElement([2, 4, 6, 8], function(num) { return num % 2 === 0; }))
+console.log(findElement([], function(num) { return num > 0; }))
